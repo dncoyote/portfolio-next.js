83ba3a6dba52a6a21ce9529e3af79a7055cefc7e
BilalThaha <bilal.athaha@gmail.com>
2023-10-20 20:17:15 +0530

    image update

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -42,7 +42,7 @@ const HeroSection = () => {
             />
           </h1>
           <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
-          Wandering through the realms of art, technology, and existence 🎨💻🌟
+          Art, Tech, and Life
           </p>
           <div>
             <Link
@@ -67,9 +67,9 @@ const HeroSection = () => {
           transition={{ duration: 0.5 }}
           className="col-span-4 place-self-center mt-4 lg:mt-0"
         >
-          <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
+          <div className="rounded-full bg-[#121212] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
             <Image
-              src="/images/hero-image.png"
+              src="/images/QWER.png"
               alt="hero image"
               className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
               width={300}
